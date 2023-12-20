e6799c2a3e6ff92100c41162f7834c9bb276e3a5
vbachh43 <vbachh.bui1611@gmail.com>
2023-12-21 00:15:28 +0700

    Setting up IOC parameter

--- Souce Code/Core/Inc/main.h
+++ Souce Code/Core/Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_YELLOW_Pin GPIO_PIN_5
+#define LED_YELLOW_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
